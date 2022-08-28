20c7e72acaaab906d1de66f65d369c3a2ca114a4
Barent Langwell <barent@Barents-MacBook-Pro.local>
2022-08-27 21:29:43 -0700

    Refactor JS logic, opt for API images over custom CSS styles

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
-async function getShuffledDeck(deckCount=1) {
+async function getNewDeck() {
+  let deckResponse = await axios.get('https://www.deckofcardsapi.com/api/deck/new/');
+  return deckResponse.data;
+}
+
+async function shuffleDeck(deckID, remaining=true) {
   try {
-    let shuffledDeckResponse = await axios.get(`https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=${deckCount}`);
-    if (!shuffledDeckResponse.data.success) {
-      throw new Error('API response indicates GET request was unsuccessful.')
+    let shuffledDeck = await axios.get(`https://www.deckofcardsapi.com/api/deck/${deckID}/shuffle/?remaining=${remaining}`);
+    if (!shuffledDeck.data.success) {
+      throw new Error('API response indicates GET request was unsuccessful')
+    } 
+    if (!shuffledDeck.data.shuffled) {
+      throw new Error('Deck not shuffled successfully!');
     }
-    let deckId = shuffledDeckResponse.data.deck_id;
-    return deckId;
-  } catch(err) {
-    console.error(`Error making GET request - ${err}`);
-    return -1;
+    return shuffledDeck.data;
+  } catch (err) {
+    console.error(`Error reshuffling deck - ${err}`);
   }
 }
 
-getShuffledDeck(1);
+async function drawFromDeck(deckID, count=1) {
+  try {
+    let drawn = await axios.get(`https://www.deckofcardsapi.com/api/deck/${deckID}/draw/?count=${count}`);
+    if (!drawn.data.success) {
+      throw new Error('Error drawing a card!');
+    }
+    let cards = drawn.data.cards;
+    for (let card of cards) {
+      console.log(card);
+      let cardImage = document.createElement('img');
+      cardImage.src = card.image;
+      document.body.appendChild(cardImage);
+    }
+    return drawn.data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function playGame() { 
+  let deck = await getNewDeck();
+  console.log(deck);
+  deck = await shuffleDeck(deck.deck_id);
+  let drawnCard = await drawFromDeck(deck.deck_id, 2);
+  console.log(drawnCard)
+}
+
+playGame();
